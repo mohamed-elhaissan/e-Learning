c2c63e84d9738d1eae7227318a8beb47430778df
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-05-20 21:31:23 +0100

    creating Register page designe And logic steps

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import {
   PiNumberCircleOneFill,
   PiNumberCircleTwoFill,
   PiNumberCircleThreeFill,
 } from "react-icons/pi";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaRegObjectGroup } from "react-icons/fa";
 
 import { FaGoogle } from "react-icons/fa";
 import { useState } from "react";
 export const RegisterPage = () => {
-    const [isFacebookHovered, setIsFacebookHovered] = useState(false);
+  const [registerSteps, setRegisterSteps] = useState(1);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (registerSteps < 3) {
+      setRegisterSteps((prev) => prev + 1);
+    }
+  };
   return (
     <div className="w-screen h-screen flex justify-center items-center ">
       <div className="p-14   w-1/3">
-        <form className="flex  flex-col gap-2 ">
+        <form className="flex  flex-col gap-2 " onSubmit={handleLogin}>
           <h1 className="text-3xl text-center">Create an account</h1>
           <p className="text-gray-500 text-center mb-10">
             Already have an account ?{" "}
@@ -41,22 +49,54 @@ export const RegisterPage = () => {
             </div>
           </div>
 
-          <div className="flex flex-col mb-1  ">
-            <label htmlFor="email" className="text-[#949494] mb-1">
-              What's your email ?{" "}
-            </label>
-            <input
-              type="email"
-              className="border-2 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-[var(--main-purple)]"
-              name="email"
-              required
-              placeholder="Enter your email address..."
-            />
-          </div>
+          <AnimatePresence>
+            {registerSteps === 1 && (
+              <div className="flex flex-col mb-1  ">
+                <label htmlFor="email" className="text-[#949494] mb-1">
+                  What's your email ?
+                </label>
+                <input
+                  type="email"
+                  className="border-2 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-[var(--main-purple)]"
+                  name="email"
+                  placeholder="Enter your email address..."
+                />
+              </div>
+            )}
+            {registerSteps === 2 && (
+              <div className="flex flex-col mb-1  ">
+                <label htmlFor="email" className="text-[#949494] mb-1">
+                  What's your Name ?
+                </label>
+                <input
+                  type="email"
+                  className="border-2 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-[var(--main-purple)]"
+                  name="email"
+                  placeholder="Enter your Name ..."
+                />
+              </div>
+            )}
+            {registerSteps === 3 && (
+              <div className="flex flex-col mb-1  ">
+                <label htmlFor="email" className="text-[#949494] mb-1">
+                  Create your Password ?
+                </label>
+                <input
+                  type="pasword"
+                  className="border-2 border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-[var(--main-purple)]"
+                  name="password"
+                  placeholder="Enter your Password ..."
+                />
+              </div>
+            )}
+          </AnimatePresence>
 
-          <button className=" bg-[#C3C3C3] rounded-full px-3 py-3 mt-5">
+          <motion.button
+            whileTap={{ scale: 0.9 }}
+            className=" bg-[#C3C3C3] cursor-pointer rounded-full px-3 py-3 mt-5"
+          >
             Next
-          </button>
+          </motion.button>
           <div className="mt-10">
             <div className="flex items-center justify-center gap-5">
               <div className="w-1/3 bg-[#C3C3C3] h-[1px] rounded-full" />
@@ -65,19 +105,17 @@ export const RegisterPage = () => {
               <div className="w-1/3 bg-[#C3C3C3] h-[1px] rounded-full" />
             </div>
             <div className="flex justify-center items-center gap-5 ">
-              <button className="rounded-full border text-sm flex transition-all duration-300 hover:bg-gradient-to-r hover:from-[#4285F4] hover:via-[#34A853] hover:via-[#FBBC05] hover:to-[#EA4335] hover:text-white px-3 py-3 mt-5">
-                <span className="text-[#333]">Sign up with Google</span>
+              <motion.button
+                whileTap={{ scale: 0.9 }}
+                className="rounded-full border text-sm cursor-pointer flex justify-center items-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#4285F4] hover:via-[#34A853] hover:via-[#FBBC05] hover:to-[#EA4335] hover:text-white px-3 py-3 mt-5"
+              >
                 <FaGoogle className="text-xl ml-2" />
-              </button>
-              <motion.button 
-              onMouseEnter={() => setIsFacebookHovered(true)}
-              onMouseLeave={() => setIsFacebookHovered(false)}
-              className=" rounded-full border text-sm flex transition-all duration-300 hover:bg-[#1877F2] hover:text-white px-3 py-3 mt-5">
-                <p className="text-[#333]">Sign up with Facebook</p>
+              </motion.button>
+              <motion.button
+                whileTap={{ scale: 0.9 }}
+                className=" rounded-full overflow-hidden cursor-pointer border text-sm flex transition-all duration-300 hover:bg-[#1877F2] hover:text-white px-3 py-3 mt-5"
+              >
                 <FaFacebookF className="text-xl ml-2" />
-                <div>
-                    
-                </div>
               </motion.button>
             </div>
           </div>
